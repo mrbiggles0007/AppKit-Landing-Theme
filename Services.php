6265b622fac541d8d8e2c1fbd091269953207025
mrbiggles0007 <42992456+mrbiggles0007@users.noreply.github.com>
2023-02-27 15:13:03 +1000

    xx

--- Services.php
+++ Services.php
@@ -1,7 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Helping MSPs structure documentation systems</title>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-4MFQK6JZKT"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-4MFQK6JZKT');
+</script>
+    <title>Document Management Services For MSPs</title>
     <!-- Meta -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
